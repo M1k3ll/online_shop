dbbc3a707004bd7aebe55b366cc5019b00846a60
Mikel <ahmadi.mikaeel@gmail.com>
2024-03-25 01:38:47 +0330

    save

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
+from accounts.forms import CustomUserCreationForm
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
